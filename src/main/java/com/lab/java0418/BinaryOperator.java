d950c18ac3923a8f958e4a69cf159b9e44e5f12e
MB-teacher <MB-teacher@192.168.1.109>
2020-04-18 12:10:52 +0800

    Java

--- src/main/java/com/lab/java0418/BinaryOperator.java
+++ src/main/java/com/lab/java0418/BinaryOperator.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.lab.java0418;
 
-/**
- *
- * @author MB-teacher
- */
 public class BinaryOperator {
-    
+    public static void main(String[] args) {
+        int no = 3;
+        int parking = 0b110101;
+        int car     = (int)Math.pow(2, no); // 0b001000;
+        int check = parking & car;
+        System.out.print("車位:" + no + "號");
+        System.out.println(check == 0 ? "可停":"不可停");
+    }
 }
