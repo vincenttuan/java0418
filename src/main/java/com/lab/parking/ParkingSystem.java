4cdda87c289f2fae4f7e911063f5404efc45508b
MB-teacher <MB-teacher@192.168.1.109>
2020-04-18 13:45:08 +0800

    Java

---
+++ src/main/java/com/lab/parking/ParkingSystem.java
@@ -0,0 +1,15 @@
+package com.lab.parking;
+
+public class ParkingSystem {
+    public static void main(String[] args) {
+        menu();
+    }
+    
+    public static void menu() { // 系統選單
+        System.out.println("1. 查詢車位");
+        System.out.println("2. 我要停車");
+        System.out.println("3. 我要取車");
+        System.out.println("4. 離開系統");
+    }
+    
+}
