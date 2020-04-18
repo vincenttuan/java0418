1156ad8b650a76f9ea32b3f2c24278a56707de82
MB-teacher <MB-teacher@192.168.1.109>
2020-04-18 10:23:24 +0800

    Java

---
+++ src/main/java/com/lab/java0418/AddOperation.java
@@ -0,0 +1,11 @@
+package com.lab.java0418;
+
+// 遞增遞減使用範例
+public class AddOperation {
+    public static void main(String[] args) {
+        int age = 10;
+        int var = ++age;
+        System.out.printf("age= %d, var= %d\n", age, var);
+    }
+ 
+}
