abb2ed1f4bfb2d2e1696b99249bc6e8193b44585
MB-teacher <MB-teacher@192.168.1.109>
2020-04-18 15:58:26 +0800

    Java

---
+++ src/main/java/com/lab/java0418/DoWhileDemo.java
@@ -0,0 +1,10 @@
+package com.lab.java0418;
+
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int count = 3;
+        do {            
+            System.out.println("Java");
+        } while (--count > 0);
+    }
+}
