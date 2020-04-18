9c6426bf817f3a0b46acd81964b4a0ad3830923b
MB-teacher <MB-teacher@192.168.1.109>
2020-04-18 11:00:39 +0800

    Java

---
+++ src/main/java/com/lab/java0418/ConditionalOperation2.java
@@ -0,0 +1,11 @@
+package com.lab.java0418;
+
+// 條件運算子
+public class ConditionalOperation2 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 0;
+        String s = (++x > 10 | y++ > 0) ? "成功" : "失敗";
+        System.out.printf("x= %d, y= %d, s= %s\n", x, y, s);
+    }
+}
