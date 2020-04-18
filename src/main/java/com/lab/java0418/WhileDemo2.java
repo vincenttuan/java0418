915f9340b4e3e1b8db45bdf1a0780e9e3ca5faeb
MB-teacher <MB-teacher@192.168.1.109>
2020-04-18 15:51:10 +0800

    Java

--- src/main/java/com/lab/java0418/WhileDemo2.java
+++ src/main/java/com/lab/java0418/WhileDemo2.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class WhileDemo2 {
     public static void main(String[] args) {
         int count = 4; // 產生 4 的 lotto 號碼
-        while(count > 0) {
+        while(count-- > 0) {
             int n = new Random().nextInt(10); // 0~9
             System.out.println(n);
-            count--;
+            //count--;
         }
     }
 }
