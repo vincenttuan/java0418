20b5cd190f0ad35f892b85f1269081e7ff5c6393
MB-teacher <MB-teacher@192.168.1.109>
2020-04-18 16:18:40 +0800

    Java

--- src/main/java/com/lab/java0418/GuessNumber.java
+++ src/main/java/com/lab/java0418/GuessNumber.java
@@ -12,6 +12,10 @@ public class GuessNumber {
             Scanner sc = new Scanner(System.in);
             System.out.printf("請在 %d ~ %d 猜一數字:\n", min, max);
             int guess = sc.nextInt();
+            // 1.檢查數值範圍是否合法 ?
+            
+            
+            // 2.進行遊戲比對
             if(guess == ans) {
                 System.out.println("恭喜答對了");
                 break;
