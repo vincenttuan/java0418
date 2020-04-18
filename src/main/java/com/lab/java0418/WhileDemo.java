8c796b3b737c61b4c70388a740ae61105cefb8f4
MB-teacher <MB-teacher@192.168.1.109>
2020-04-18 15:46:33 +0800

    Java

--- src/main/java/com/lab/java0418/WhileDemo.java
+++ src/main/java/com/lab/java0418/WhileDemo.java
@@ -6,9 +6,12 @@ public class WhileDemo {
     public static void main(String[] args) {
         while(true) {
             int n = new Random().nextInt(100); // 0~99
+            if(n != 0 && n % 3 != 0) { // n!= 0 且 n 不是 3 的倍數
+                continue; // 重新執行迴圈
+            }
             System.out.println(n);
             if(n == 0) {
-                break;
+                break; // 跳離迴圈
             }
         }
     }
