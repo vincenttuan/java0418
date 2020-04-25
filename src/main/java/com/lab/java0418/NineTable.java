5c558ab0c0bbea79da5a2fca442c6cb1ea01d996
MB-teacher <MB-teacher@192.168.1.109>
2020-04-25 10:41:54 +0800

    Java

--- src/main/java/com/lab/java0418/NineTable.java
+++ src/main/java/com/lab/java0418/NineTable.java
@@ -2,11 +2,10 @@ package com.lab.java0418;
 
 public class NineTable {
     public static void main(String[] args) {
-        
-        
-        for(int x=1;x<=9;x++){
-            for (int y = 1; y <= 9; y++) {
-                System.out.printf("%d*%d=%d\t", x, y, (x * y));
+        int n = 11;
+        for(int x=1;x<=n;x++){
+            for (int y = 1; y <= n; y++) {
+                System.out.printf("%02d*%02d=%-3d ", x, y, (x * y));
             }
             System.out.println();
         }
