6fd0e500880be63f126c5d219d7dcafdfebd244b
MB-teacher <MB-teacher@192.168.1.109>
2020-04-25 10:26:49 +0800

    Java

--- src/main/java/com/lab/java0418/ForDemo_PrimeNumber.java
+++ src/main/java/com/lab/java0418/ForDemo_PrimeNumber.java
@@ -2,9 +2,19 @@ package com.lab.java0418;
 
 public class ForDemo_PrimeNumber {
     public static void main(String[] args) {
-        int n = 17;
-        System.out.printf("%d 是否是質數? %b\n", n, checkPrime(n));
+        //int n = 17;
+        //System.out.printf("%d 是否是質數? %b\n", n, checkPrime(n));
+        printPrimeByRange(2, 100);
     }
+    
+    public static void printPrimeByRange(int begin, int end) {
+        for(int i=begin;i<=end;i++) {
+            if(checkPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+    
     public static boolean checkPrime(int n) {
         for(int i=2;i<=n/2;i++){
             if(n % i == 0){
@@ -13,4 +23,5 @@ public class ForDemo_PrimeNumber {
         }
         return true;
     }
+    
 }
